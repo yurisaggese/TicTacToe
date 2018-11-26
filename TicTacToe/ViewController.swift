258ace3e30bcdb777973262840d39c7d586be7d6
Yuri Saggese <yurisaggese@gmail.com>
2018-11-26 17:18:15 +0100

    aaa

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        print("This is a Git tutorial")
+    }
 
 }
 
